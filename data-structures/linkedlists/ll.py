f31afb03f10e4b35623f4184c90fc27f2eb0a23e
jeygen <83786292+jeygen@users.noreply.github.com>
2022-08-14 15:31:55 -0400

    Update ll.py

--- data-structures/linkedlists/ll.py
+++ data-structures/linkedlists/ll.py
@@ -27,14 +27,42 @@ class ll:
         self.head = None
 
     def printLL(self):
-        while (self.head is not None):
-            print(self.head.data)
-            self.head = self.head.next
+        temp = self.head
+        while (temp is not None):
+            print(temp.data)
+            temp = temp.next
 
-    def addBegin(self, data=none):
+    def addBegin(self, data):
         n = node(data)
         n.next = self.head
         self.head = n
+    
+    def deleteLL(self, data=None):
+        if (self.head is not None):
+            if (self.head.data == data):
+                self.head = self.head.next
+                self.head = None
+                return
+        while (self.head is not None):
+            if self.head.data == data:
+                break
+            prev = self.head
+            self.head = self.head.next
+
+        if (self.head == None):
+            return
+
+        prev.next = self.head.next
+        self.head = None
 
 list = ll()
-list.head = node
+list.head = node(0)
+for i in range(1,6):
+    list.addBegin(i)
+list.printLL()
+#print('=======')
+list.printLL()
+list.deleteLL(3)
+list.printLL()
+
+
