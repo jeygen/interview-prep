8a17a68036ef5acb30a1d0429bf7ef25f10b5a63
Surface Bob <joshua.eygenraam@ryerson.ca>
2022-08-19 12:32:50 -0400

    Added binary search

---
+++ search/binarySearch.c
@@ -0,0 +1,47 @@
+/*
+Binary Search
+T: O(n)
+For sorted arrays
+
+    binarySearch(arr, x, low, high)
+           if low > high
+               return False 
+   
+           else
+               mid = (low + high) / 2 
+                   if x == arr[mid]
+                   return mid
+       
+               else if x > arr[mid]        // x is on the right side
+                   return binarySearch(arr, x, mid + 1, high)
+               
+               else                        // x is on the right side
+                   return binarySearch(arr, x, low, mid - 1) 
+*/
+
+#include <stdio.h>
+
+int binary_Search(int* arr, int x, int low, int high) {
+	int mid = (low + high) / 2;
+	
+	if (low > high) {
+		fprintf(stderr, "\nMistake in given range\n");
+		return 0;
+	}
+	else if (x == arr[mid]) {
+		return mid;
+	}
+	else if (x > arr[mid]) {
+		binary_Search(arr, x, mid+1, high);
+	}
+	else {
+		binary_Search(arr, x, low, mid-1);
+	}		
+}
+
+int main(void) {
+	int a[] = {1,44,88,2341,12343,132413,1241212};
+	int length = sizeof(a)/sizeof(a[0]);
+	printf("Element position is %d\n", binary_Search(a, 88, 0, length-1)); 
+	return 0;
+}
