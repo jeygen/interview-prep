879a8b4dc073ae0c50093320c2fe76a5ef98009a
Surface Bob <joshua.eygenraam@ryerson.ca>
2022-08-25 14:43:35 -0400

    updated Sorts.java

--- sorts/sorts.java
+++ sorts/sorts.java
@@ -1,6 +1,6 @@
 public class Sorts {
 	
-	void bubbleSort(int[] arr) {
+	int[] bubbleSort(int[] arr) {
 		int i, j, temp;
 		for (i=0; i<arr.length-1; i++) {
 			for (j=0; j<arr.length-1-i; j++) {
@@ -11,6 +11,7 @@ public class Sorts {
 				}
 			}
 		}
+	return arr;
 	}
 						
 					
