b2cbbcd495e2213c5078265d9ff61ca6cc30560f
jeygen <83786292+jeygen@users.noreply.github.com>
2022-10-23 22:15:59 -0400

    added some more

--- sorts/sorts.py
+++ sorts/sorts.py
@@ -15,3 +15,32 @@ s = sorts()
 arr = s.bubbleSort(arr)
 for i in range(len(arr)):
     print("% d" % arr[i], end=" ")
+
+    def countingSort(arr):
+        # Write your code here
+        c = 0
+        k = 0
+        arr.sort()
+        arr2 = []
+        '''
+        while (c < len(arr)):
+            for i in range(len(arr)):
+                if arr[i] == c:
+                k += 1
+            arr2.append(k)
+            
+            k = 0
+            c += 1
+        '''
+        for i in range(len(arr)):
+            arr2.append(arr.count(i))
+        
+        return arr2
+
+    # smarter way
+    def countingSort2(arr):
+        tot = [0]*100
+        for j in range(0,n):
+            temp = arr[j]
+            tot[temp] += 1
+        return tot
--- stats_math/fizzbuzz.py
+++ stats_math/fizzbuzz.py
@@ -11,3 +11,15 @@ def fizzbuzz(n):
 def fizzbuzz2(n):
     return "FizzBuzz" if n % 3 == 0 and n % 5 == 0 else "Fizz" if n % 3 == 0 else "Buzz" if n % 5 == 0 else n
 
+
+    
+def fizzBuzz3(n):
+    for i in range(1,n+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print ("FizzBuzz")
+        elif i % 3 == 0:
+            print( "Fizz")
+        elif i % 5 == 0:
+            print ("Buzz")
+        else:
+            print (i)
